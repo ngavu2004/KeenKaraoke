4c458410ad2fae5882b8c1e01c95e1206cd5cc86
Vikriti Lokegaonkar <v@lokegaonkar.in>
2024-09-28 16:12:14 -0700

    Added a fastapi server wih GET /fetch_audio

---
+++ Backend/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+import youtube_dl
+import random
+import string
+import ffmpeg
+from spleeter.separator import Separator
+
+app = FastAPI()
+
+@app.get("/fetch_audio")
+async def fetch_audio(youtube_url: str):
+    filename = "".join(random.choices(string.ascii_letters, k=8)) + ".mp4"
+    ydl_opts = {
+        "format": "mp4",
+        "verbose": True,
+        "outtmpl": filename
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([youtube_url])
+
+    audio = ffmpeg.input(filename).audio
+
+    prediction = separator.separate(audio)
+    print(prediction)
+    #ffmpeg.output(audio, filename+".mp3").run()
