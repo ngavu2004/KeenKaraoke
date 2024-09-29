60bf95e4b1ab95a636c6bff1ab4bf879c654a095
Vikriti Lokegaonkar <v@lokegaonkar.in>
2024-09-29 06:11:06 -0700

    Added splitter code

---
+++ Splitter/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, UploadFile, StaticFiles
+import random
+import string
+import demucs.separate
+import os
+
+app = FastAPI()
+
+import os
+if not os.path.exists("static"):
+    os.makedirs("static")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.post("/split_audio")
+async def split_audio(source_audio: UploadFile):
+    filename = "".join(random.choices(string.ascii_letters, k=8)) + ".mp3"
+    with open(filename, "w") as f:
+        f.write(source_audio.read())
+
+    demucs.separate.main(["--mp3", "--two-stems", "vocals", "-n", "mdx_extra", filename, "-o", filename])
+
+    os.rename(filename + "/mdx_extra/song/no_vocals.mp3", "static/" + filename)
+    return "static/" + filename
